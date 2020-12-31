86880bc1d49378905489afdc09105a750e267192
pepdiz <socialkuma@hotmail.com>
2020-12-31 18:56:34 +0100

    add constant for array limits and check functions

--- csvff.sh
+++ csvff.sh
@@ -11,6 +11,12 @@ sintaxis () {
   exit
 }
 
+check_file_cols () {
+  OK_LINES=$(awk -F, "NF==$2{print NF}" < $1 |wc -l)
+  FILE_LINES=$(wc -l < $1) 
+  [ "$OK_LINES" -eq "$FILE_LINES" ] 
+}
+
 bas-array () {
   if (( LINES > 0 && COLS > 0 )) ; then
 	  let LINES--
@@ -22,13 +28,17 @@ bas-array () {
 
 c-array () {
   if (( LINES > 0 && COLS > 0 )) ; then
-	  echo "unsigned char $V[$LINES][$COLS] = { _"
+    echo "#define ${V}_ROWS $LINES"
+    echo "#define ${V}_COLS $COLS"
+	  echo "unsigned char $V[${V}_ROWS][${V}_COLS] = { _"
 	  sed -r -e '$s/^[^,]*(([0-9]+ *, *)+[0-9]+).*/{\1}\n};/;$!s/^[^,]*(([0-9]+ *, *)+[0-9]+).*/{\1} ,/'
 #	  sed -r -e '$s/.* (([0-9]+,)+[0-9]+)/{\1}\n};/;$!s/.* (([0-9]+,)+[0-9]+)/{\1} ,/'
   fi
 }
 
 asm-bytes () {
+  echo "${V}_ROWS EQU $LINES"
+  echo "${V}_COLS EQU $COLS" 
   echo "$V:"
   sed -r -e 's/^[^,]*(([0-9]+ *, *)+[0-9]+).*/db \1/'
 #  sed -r -e 's/.* (([0-9]+,)+[0-9]+)/db \1/'
@@ -50,6 +60,8 @@ V=${3:-byteSet}
 
 COLS=$(grep -E -o '([0-9]+ *, *)+[0-9]+' $2 | awk -F, 'NR==1{max=NF}NF > max{max=NF}END{print max}')
 LINES=$(wc -l < $2)
+
+check_file_cols $2 $COLS || { echo "number of fields in file $2 are not the same in all lines"; exit 2; }
 
 case $1 in
 bas)	dos2unix < $2 | bas-array ;;
